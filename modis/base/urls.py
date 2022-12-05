5937ade85d2b977c72679ee742d42cd0c979999f
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2022-12-05 20:36:23 +0100

    Add several views and the corresponding html files.

---
+++ modis/base/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('module/<str:primary_key>/', views.module, name='module'),
+    path('create-module/', views.create_module, name='create-module'),
+    path('update-module/<str:primary_key>/', views.update_module, name='update-module'),
+    path('delete-module/<str:primary_key>/', views.delete_module, name='delete-module'),
+]
--- modis/base/views.py
+++ modis/base/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Module
+from .forms import ModuleForm
 
-# Create your views here.
+
+def home(request):
+    search_query = request.GET.get('q') if request.GET.get('q') is not None else ''
+    modules = Module.filter_modules_by_search_query(search_query)
+    module_count = modules.count()
+
+    context = {'modules': modules,
+               'module_count': module_count}
+    return render(request, 'home.html', context)
+
+
+def module(request, primary_key):
+    module = Module.objects.get(id=primary_key)
+    context = {'module': module}
+    return render(request, 'module.html', context)
+
+
+def create_module(request):
+    form = ModuleForm()
+
+    if request.method == 'POST':
+        form = ModuleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'module_form.html', context)
+
+
+def update_module(request, primary_key):
+    module = Module.objects.get(id=primary_key)
+    form = ModuleForm(instance=module)
+
+    if request.method == 'POST':
+        form = ModuleForm(request.POST, instance=module)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'module_form.html', context)
+
+
+def delete_module(request, primary_key):
+    module = Module.objects.get(id=primary_key)
+
+    if request.method == 'POST':
+        module.delete()
+        return redirect('home')
+
+    context = {'object': module}
+    return render(request, 'delete.html', context)
