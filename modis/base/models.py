d05966f3446b2029882932810baa12b35e69979e
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2022-12-22 13:16:37 +0100

    Add filter by category functionality

--- modis/base/models.py
+++ modis/base/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.db.models import Q
+import pandas as pd
 
 # get the custom user model
 User = get_user_model()
@@ -65,7 +66,7 @@ class Module(models.Model):
         return self.title
 
     @staticmethod
-    def filter_modules_by_search_query(search_query):
+    def find_modules_by_search_query(search_query):
         return Module.objects.filter(
             Q(title__icontains=search_query) |
             Q(content__icontains=search_query) |
@@ -83,6 +84,36 @@ class Module(models.Model):
         if direction == 'desc':
             return modules.order_by('-title').values()
         return modules.order_by('title').values()
+
+
+    @staticmethod
+    def filter_modules_by_semester(semester):
+        semester = Semester.objects.filter(type=semester)
+        return Module.objects.filter(semesters__in=semester)
+
+    @staticmethod
+    def filter_modules_by_graduate_program(graduate_program):
+        one = graduate_program.replace('(', '')
+        two = one.replace(')','')
+        title, degree = two.rsplit(' ', 1)
+
+        graduate_program = GraduateProgram.objects.filter(title=title, degree=degree)
+        return Module.objects.filter(graduate_programs__in=graduate_program)
+
+    @staticmethod
+    def filter_modules_by_specialisation_track(specialisation_track):
+        specialisation_track = SpecialisationTrack.objects.filter(title=specialisation_track)
+        return Module.objects.filter(specialisation_tracks__in=specialisation_track)
+
+    @staticmethod
+    def filter_modules_by_credits(credits):
+        return Module.objects.filter(credits=credits)
+
+    @staticmethod
+    def filter_modules_by_lecturer(lecturer):
+        first_name, last_name = lecturer.split(' ')
+        lecturer = Lecturer.objects.filter(first_name=first_name, last_name=last_name)
+        return Module.objects.filter(lecturer__in=lecturer)
 
 
 class Student(models.Model):
--- modis/base/views.py
+++ modis/base/views.py
@@ -1,29 +1,19 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
-from .models import Module, Student
+from .models import Module, Student, Semester, SpecialisationTrack, Lecturer, GraduateProgram
 from .forms import ModuleForm
 
-
 modules = Module.objects.all()
+
 
 @login_required
 def home(request):
     global modules
     search_query = request.GET.get('q') if request.GET.get('q') is not None else ''
-    modules = Module.filter_modules_by_search_query(search_query)
+    modules = Module.find_modules_by_search_query(search_query)
+    semesters = Semester.objects.all()
 
-    context = {'modules': modules,
-               'module_count': modules.count()
-               }
-
-    try:
-        student = Student.objects.get(user=request.user)
-    except:
-        student = None
-
-    if student is not None:
-        context['student'] = student
-
+    context = get_context_for_home(request)
     return render(request, 'home.html', context)
 
 
@@ -33,10 +23,7 @@ def module(request, primary_key):
 
     context = {'module': module}
 
-    try:
-        student = Student.objects.get(user=request.user)
-    except:
-        student = None
+    student = get_student_for_context(request)
 
     if student is not None:
         context['student'] = student
@@ -93,7 +80,7 @@ def toggle_favourites(request, module_id):
         student.favourites.remove(module)
     else:
         student.favourites.add(module)
-    return redirect(request. META. get('HTTP_REFERER', '/'))
+    return redirect(request.META.get('HTTP_REFERER', '/'))
 
 
 @login_required()
@@ -105,28 +92,72 @@ def favourites(request):
 
 
 @login_required()
-def set_sorting_direction(request, direction):
+def sort(request, direction):
     global modules
     modules = Module.sort_modules_by_title(modules, direction)
 
+    context = get_context_for_home(request)
+
+    return render(request, 'home.html', context)
+
+
+@login_required()
+def filter(request):
+    global modules
+    semester = request.GET.get('semester')
+    graduate_program = request.GET.get('program')
+    specialisation_track = request.GET.get('track')
+    credits = request.GET.get('credits')
+    lecturer = request.GET.get('lecturer')
+
+
+    if semester is not None:
+        modules = Module.filter_modules_by_semester(semester)
+    elif specialisation_track is not None:
+        modules = Module.filter_modules_by_specialisation_track(specialisation_track)
+    elif credits is not None:
+        modules = Module.filter_modules_by_credits(credits)
+    elif graduate_program is not None:
+        modules = Module.filter_modules_by_graduate_program(graduate_program)
+    elif lecturer is not None:
+        modules = Module.filter_modules_by_lecturer(lecturer)
+    else:
+        modules = None
+
+    context = get_context_for_home(request)
+
+    return render(request, 'home.html', context)
+
+
+@login_required()
+def get_context_for_home(request):
+    semesters = Semester.objects.all()
+    specialisation_tracks = SpecialisationTrack.objects.all()
+    credits = set(Module.objects.values_list('credits', flat=True))
+    lecturers = Lecturer.objects.all()
+    student = get_student_for_context(request)
+    graduate_programs = GraduateProgram.objects.all()
+
     context = {'modules': modules,
-               'module_count': modules.count()
+               'module_count': modules.count(),
+               'semesters': semesters,
+               'specialisation_tracks': specialisation_tracks,
+               'credits': credits,
+               'lecturers': lecturers,
+               'graduate_programs': graduate_programs
                }
 
+    if student is not None:
+        context['student'] = student
+
+    return context
+
+
+@login_required()
+def get_student_for_context(request):
     try:
         student = Student.objects.get(user=request.user)
     except:
         student = None
 
-    if student is not None:
-        context['student'] = student
-
-    return render(request, 'home.html', context)
-
-
-
-
-
-
-
-
+        return student
