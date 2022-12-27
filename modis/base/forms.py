94ffbc5fb402cb5fba3777334011cc08ddc73053
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2022-12-27 13:22:48 +0100

    Adjust styling/content of module form

--- modis/base/forms.py
+++ modis/base/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Module, User, Lecturer
 
@@ -6,10 +7,28 @@ class ModuleForm(ModelForm):
     class Meta:
         model = Module
         fields = '__all__'
+        widgets = {
+            'semesters': forms.CheckboxSelectMultiple,
+            'graduate_programs': forms.CheckboxSelectMultiple,
+            'specialisation_tracks': forms.CheckboxSelectMultiple,
+        }
+        labels = {
+            'title': 'Titel',
+            'content': 'Inhalt',
+            'learning_objective': 'Lernziele',
+            'lecturer': 'Verantwortlicher',
+            'semesters': 'Semester',
+            'graduate_programs': 'Studiengänge',
+            'specialisation_tracks': 'Vertiefungsanrechnung',
+            'credits': 'Credits'
+        }
 
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user')
         super(ModuleForm, self).__init__(*args, **kwargs)
+
+        for field in self.fields.values():
+            field.error_messages = {'required': 'Dies ist ein Pflichtfeld.'}
 
         if user.user_type != user.ADMIN:
             self.fields.pop('lecturer')
