69062c72d88bd619f18ecabd495713590a528e4c
Andreas Schmid <andreas.schmid1@hs-augsburg.de>
2022-12-30 15:47:56 +0100

    Add minor changes to bootstrap styling.

--- modis/modis/urls.py
+++ modis/modis/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
@@ -5,4 +7,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('base.urls')),  # Get all paths from base.urls
     path('', include('accounts.urls')),  # Get all paths from accounts.urls
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
